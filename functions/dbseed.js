240f176456c75612ea49e358f361933847e3153b
Mattias S <matte_1500@hotmail.com>
2022-11-05 18:16:34 +0100

    Resolve  dbseed function

--- functions/dbseed.js
+++ functions/dbseed.js
@@ -5,32 +5,32 @@ import {saveScrapedFuelPrices} from "./src/saveScrapedFuelPrices.js"
 import {saveCalculatedFuelPrice} from "./src/saveCalculatedFuelPrice.js"
 import {calculateFuelPrice} from "./src/calculateFuelPrice.js"
 
-admin.initializeApp({
-	databaseURL: "http://127.0.0.1:9000/?ns=fuel-price-viewer-default-rtdb",
-})
+(async () => {
+	admin.initializeApp({
+		databaseURL: "http://127.0.0.1:9000/?ns=fuel-price-viewer-default-rtdb",
+	})
 
-const db = admin.database()
-db.useEmulator("localhost", 9000)
+	const db = admin.database()
+	db.useEmulator("localhost", 9000)
+	const timestamp = 0
+	await Promise.all(fuelTypes.map(async (fuelType) => {
+		const fakeFuelPrices = Array.from({length: 10}, () => ({
+			price: faker.datatype.number({min: 28, max: 32}),
+			company: faker.company.name(),
+			municipality: faker.address.county(),
+			address: faker.address.streetAddress(),
+			fuelType: fuelType,
+		}))
 
-const timestamp = 0
-await Promise.all(fuelTypes.map(async (fuelType) => {
-	const fakeFuelPrices = Array.from({length: 10}, () => ({
-		price: faker.datatype.number({min: 28, max: 32}),
-		company: faker.company.name(),
-		municipality: faker.address.county(),
-		address: faker.address.streetAddress(),
-		fuelType: fuelType,
+		await saveScrapedFuelPrices(
+			db,
+			timestamp,
+			fakeFuelPrices,
+			fuelType,
+		)
+
+		const fuelPrice = await calculateFuelPrice(db, fuelType)
+		await saveCalculatedFuelPrice(db, timestamp, fuelPrice, fuelType)
 	}))
+})().then((_) => process.exit())
 
-	await saveScrapedFuelPrices(
-		db,
-		timestamp,
-		fakeFuelPrices,
-		fuelType,
-	)
-
-	const fuelPrice = await calculateFuelPrice(db, fuelType)
-	await saveCalculatedFuelPrice(db, timestamp, fuelPrice, fuelType)
-}))
-
-process.exit()
